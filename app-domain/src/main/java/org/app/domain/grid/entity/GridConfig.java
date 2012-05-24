d33b3098d7a13df9a46a45e13ae89ea9bb4e5677
Swaron Chen <swaron.chen@gmail.com>
2012-05-24 17:32:29 +0800

    rename app-domain1 to app-domain

---
+++ app-domain/src/main/java/org/app/domain/grid/entity/GridConfig.java
@@ -0,0 +1,43 @@
+package org.app.domain.grid.entity;
+
+import java.util.List;
+
+public class GridConfig {
+	String tableName;
+	String entityName;
+	String idProperty;
+	List<ColumnConfig> columns;
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+	public String getEntityName() {
+		return entityName;
+	}
+
+	public void setEntityName(String entityName) {
+		this.entityName = entityName;
+	}
+
+	public String getIdProperty() {
+		return idProperty;
+	}
+
+	public void setIdProperty(String idProperty) {
+		this.idProperty = idProperty;
+	}
+
+	public List<ColumnConfig> getColumns() {
+		return columns;
+	}
+
+	public void setColumns(List<ColumnConfig> columns) {
+		this.columns = columns;
+	}
+
+}
